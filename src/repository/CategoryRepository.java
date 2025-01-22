5c8b80a740e36a023e4b394126e744922e42e32b
gsd123236 <gauravdhotre2009@gmail.com>
2025-01-22 11:07:35 +0530

    Add files via upload

---
+++ src/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.Excel.repository;
+
+import com.Excel.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
+}
